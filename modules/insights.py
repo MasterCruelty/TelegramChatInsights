5c7c812d3b54c734c2dc6dfcbc0752334a031b01
Roberto <roberto.antoniello@studenti.unimi.it>
2024-04-07 16:08:17 +0200

    :construction: :wrench: finishing piechart

--- modules/insights.py
+++ modules/insights.py
@@ -3,7 +3,7 @@ import time
 import sys
 sys.path.append(sys.path[0] + "/..")
 from utils.get_config import *
-from utils.sysfunctions as usys import count_messages
+import utils.dbfunctions as udb
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
@@ -18,22 +18,22 @@ plt.style.use('fivethirtyeight')
     Plot piechart for personal chats
 """
 def piechart(client,message,query):
-    
     #fetch chat id/first name
     ids,first_names,message_counts = udb.fetch_chat_info()
-    #get current message count for every chat
-
-    #save record for every chat in db with current date
-
+    
+    #get and save current message count for every chat in db
+    for i in range(len(ids)):
+        udb.update_chat_data(client,message,ids[i])
+    
     #prepare piechart as image in RAM
     plt.clf()
     temp = io.BytesIO()
     plt.figure(figsize=(20,15))
-    colours = [tuple(np.random.choice(range(256), size=3)/256) + (1,) for n in range(len(labels))]
-    #plt.pie(values,labels=labels,colors=colours)
+    colours = [tuple(np.random.choice(range(256), size=3)/256) + (1,) for n in range(len(ids))]
+    plt.pie(message_counts,labels=first_name,colors=colours)
     plt.savefig(temp,format='png')
     temp.seek(0)
     image_file = temp
-    #sendPhoto(client,message,image_file,'__Ecco il grafico a torta prodotto__')
+    sendPhoto(client,message,image_file,'__Here a piechart of your personal chats.__')
     temp.close()
     image_file.close()
