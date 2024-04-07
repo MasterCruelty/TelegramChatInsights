6f489cc55e13daee7bf6e2edc4d8c6ba7cd27aac
Roberto <roberto.antoniello@studenti.unimi.it>
2024-04-07 17:48:38 +0200

    :recycle: :wrench: refactor and supp function

--- modules/insights.py
+++ modules/insights.py
@@ -15,18 +15,20 @@ matplotlib.use('Agg')
 #set matplotlib style
 plt.style.use('fivethirtyeight')
 
+
+def count_all_msg(client,message,ids):
+    result = []
+    for i in range(len(ids)):
+        result.append(usys.count_messages(client,message,ids[i]))
+    return result
+
 """
     Plot piechart for personal chats
 """
 def piechart(client,message,query):
     #fetch chat id/first name
     ids,first_names = udb.fetch_chat_info()
-    message_counts = []
-    
-    #get and save current message count for every chat in db
-    for i in range(len(ids)):
-        message_counts.append(usys.count_messages(client,message,ids[i]))
-        udb.update_chat_data(client,message,ids[i])
+    message_counts = count_all_msg(client,message,ids)
     
     #prepare piechart as image in RAM
     plt.clf()
@@ -40,3 +42,14 @@ def piechart(client,message,query):
     sendPhoto(client,message,image_file,'__Here a piechart of your personal chats.__')
     temp.close()
     image_file.close()
+
+"""
+    Update data on db about Personal Chats 
+"""
+def new_check_data(client,message,query):
+    ids,first_names = udb.fetch_chat_info()
+    message_counts = count_all_msg(client,message,ids)
+    #get and save current message count for every chat in db
+    for i in range(len(ids)):
+        udb.update_chat_data(client,message,ids[i])
+    sendMessage(client,message,"__Nuova analisi eseguita in data " + datetime.datetime.now().strftime('%d-%m-%Y'))
